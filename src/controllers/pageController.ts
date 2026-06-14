4fd6331d4e3072c2b62d11f5e6037a5c9f23d8f3
Thais Santos <thaislee77@gmail.com>
2026-06-14 20:27:36 -0300

    finalização -v 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,26 +1,63 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet.ts'
+import { createMenuObject } from '../helpers/createMenuObject.ts';
 
 export const home = (req: Request, res: Response) => {
-    res.send('home controller');
-    // res.render('pages/page');
+
+    let list = Pet.getAll();
+
+    //res.send('home controller');
+     res.render('pages/page',{
+        menu:  createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg',
+        },
+
+        list
+
+     });
 
 }
 
+
 export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog');
+     res.render('pages/page',{
+        menu:  createMenuObject('dog'),
+        banner: {
+            title: 'Todos os cachorros',
+            background: 'banner_dog.jpg',
+        },
+        list
+     });
 
-
-    // res.render('pages/page');
 
 }
 
 export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType('cat');
+    res.render('pages/page',{
+        menu:  createMenuObject('cat'),
+        banner: {
+            title: 'Todos os gatos',
+            background: 'banner_cat.jpg',
+        },
 
-    // res.render('pages/page');
+        list
+     });
 
 }
 
 export const fishes = (req: Request, res: Response) => {
-
-    // res.render('pages/page');
+    let list = Pet.getFromType('fish');
+   res.render('pages/page',{
+      menu:  createMenuObject('fish'),
+        banner: {
+            title: 'Todos os peixes',
+            background: 'banner_fish.jpg',
+        },
+        list
+     });
 
 }
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet.ts';
+import { createMenuObject } from '../helpers/createMenuObject.ts';
 
 export const search = (req: Request, res: Response) => {
 
     // res.render('pages/search');
+ let query: string = req.query.q as string;
+
+ if(!query) {
+    res.redirect('/');
+    return;
+ }
+
+ let list = Pet.getFromName(query);
+    res.render('pages/page', {
+        menu:createMenuObject(''),
+        list,
+        query
+    });
 
 }
